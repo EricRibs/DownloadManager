d88793fd9e9b9353eaf84bd8d78512340a8f3770
Eric <57811230+EricRibs@users.noreply.github.com>
2019-12-06 08:50:16 -0300

    first commit

---
+++ DownloadManager/frmMain.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadManager
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            using (frmSetting frm = new frmSetting())
+            {
+                frm.ShowDialog();
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            using (frmAddUrl frm = new frmAddUrl())
+            {
+                if(frm.ShowDialog() == DialogResult.OK)
+                {
+                    frmDownload frmDownload = new frmDownload(this);
+                    frmDownload.Url = frm.Url;
+                    frmDownload.Show();
+                }
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure want to delete this record ?", "Message",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                for(int i = listView1.SelectedItems.Count; i > 0; i--)
+                {
+                    ListViewItem item = listView1.SelectedItems[i - 1];
+                    App.DB.Files.Rows[item.Index].Delete();
+                    listView1.Items[item.Index].Remove();
+                }
+                //Sabe
+                App.DB.AcceptChanges();
+                App.DB.WriteXml(string.Format("{0}/data.dat", Application.StartupPath));
+            }
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            string fileNames = string.Format("{0}/data.dat", Application.StartupPath);
+            if (File.Exists(fileNames))
+                App.DB.ReadXml(fileNames);
+            foreach(Database.FilesRow  row in App.DB.Files)
+            {
+                ListViewItem item = new ListViewItem(row.Id.ToString());
+                item.SubItems.Add(row.Url);
+                item.SubItems.Add(row.FileName);
+                item.SubItems.Add(row.FileSize);
+                item.SubItems.Add(row.DateTime.ToLongDateString());
+                listView1.Items.Add(item);
+            }
+        }
+    }
+}
